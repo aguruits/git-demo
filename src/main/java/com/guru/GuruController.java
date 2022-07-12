e5a8ee9c637f9549e624d7ca5a7b8526fe2fdac1
Gurumurthy <guru@guru.com>
2022-07-12 15:28:26 +0530

    Guru added new branch Branch_Guru

---
+++ src/main/java/com/guru/GuruController.java
@@ -0,0 +1,14 @@
+package com.guru;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GuruController {
+	
+	@GetMapping("/getGuruMessage")
+	public String getMessage(){
+		return "Hi Guru, Welcome to DevopsDemo Lab. Please continue you learnings";
+	}
+	
+}
